6cf55a09aa9bcee745954f5ea524c58db44d02f9
Azakana <danielsaputradivo@gmail.com>
2024-06-28 15:02:23 +0700

    Alhamdulillah belajar sesuatu hari ini :)

--- Python_Basic/4Konversi.py
+++ Python_Basic/4Konversi.py
@@ -1,1 +1,17 @@
 # Durasi Materi: 31:06
+Name = input('Masukkan Nama Anda: ')
+Year = input('Masukkan Tahun Lahir Anda: ')
+currentYear = input('Masukkan Tahun Sekarang: ')
+#print(type(Year))
+currentYear = int(currentYear)
+Year = int(Year)
+# print(type(Year))
+
+umurUserSekarang = currentYear - Year
+print(f""" 
+Hai {Name}  
+Umur Kamu Sekarang adalah: {umurUserSekarang} 
+dan kamu lahir pada {Year}
+ """)
+
+#Cara mengkonversi itu gampang njir, cuman perlu <Tipe Data>(<Nama Variabel>), misal int(Nilai)
---
+++ Python_Basic/5String.py
@@ -0,0 +1,7 @@
+# Durasi Materi: 39:58
+Nama = 'Divo Saputra'
+print(Nama[11]) # Akan mengambil index ke 11(a)
+print(Nama[-12]) # Akan mengambil index ke -12(D)
+
+print(Nama[0:4]) # akan mencetak index dari 0-4 (Divo), dan (:) artinya kayak jarak gitu deh
+print(Nama[5:]) # maksudnya tuh kayak ngambil index ke 5 sampe akhir
