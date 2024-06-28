267a6936536efc6e30c5a9ce3b07754d01cb653d
Azakana <danielsaputradivo@gmail.com>
2024-06-28 23:28:49 +0700

    .

---
+++ Python_Basic/7StringMethod.py
@@ -0,0 +1,11 @@
+#Durasi Materi: 54:18
+Belajar = 'Belajar Python Sama Ayang'
+
+# String Method
+print(len(Belajar))
+print(Belajar.upper())
+print(Belajar.lower())
+print(Belajar.replace("Python", "Ruby"))
+
+print(f"Hai nama saya Divo, dan saya sedang {Belajar.title()}")
+print(f"Hai nama saya Divo, dan saya sedang {Belajar.replace("Belajar", "Sinau")}")
