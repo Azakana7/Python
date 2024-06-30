e38e0cf2e97002a48ced92518fe381e45c261165
Azakana7 <danielsaputradivo@gmail.com>
2024-06-30 09:53:30 +0700

    .

--- Python_Basic/5String.py
+++ Python_Basic/5String.py
@@ -1,6 +1,6 @@
 # Durasi Materi: 39:58
 Nama = 'Divo Saputra'
-print(Nama[11]) # Akan mengambil index ke 11(a)
+print(Nama[10]) # Akan mengambil index ke 11(a)
 print(Nama[-12]) # Akan mengambil index ke -12(D)
 
 print(Nama[0:4]) # akan mencetak index dari 0-4 (Divo), dan (:) artinya kayak jarak gitu deh
