a040f9bdc882422d7cc9b145a95c3100fc5babbc
Azakana <danielsaputradivo@gmail.com>
2024-06-30 11:50:47 +0700

    Learning Something New Today :)

---
+++ Python_Basic/10PercabanganIF.py
@@ -0,0 +1,8 @@
+
+#! Durasi: 01:16:44
+is_Day = False
+
+if is_Day:
+    print("Selamat siang dunia!")
+else:
+    print("Ini kapan njir")
--- Python_Basic/9MathModule.py
+++ Python_Basic/9MathModule.py
@@ -1,1 +1,7 @@
 #Durasi: 01:12:13
+import math
+Q = 5.9
+#print(round(Q)) #* Round digunakan untuk membulatkan angka
+print(math.ceil(Q)) #? Memaksa untuk membulatkan angka meskipun nilai dibelakang (,) tidak diatas 4
+print(math.floor(Q)) #!Kebalikan dari Ceil lah intinya (Njir)
+
