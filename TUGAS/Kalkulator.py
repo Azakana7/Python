ed74e9549760c58f5496f0fdaea2915fcdb093f3
Azakana <danielsaputradivo@gmail.com>
2024-07-04 00:40:49 +0700

    .

---
+++ TUGAS/Kalkulator.py
@@ -0,0 +1,31 @@
+
+#? 
+Command = ""
+
+while Command != "exit":
+    Command = input("Pilih Operasi: ")
+
+    if Command == "exit":
+        break #! Memaksa program untuk berhenti
+
+    if Command != "+" and Command != "-" and Command != "*" and Command != "/":
+        print("Perintah tidak dikenali!")
+        continue #! Memaksa untuk mengulangi perulangan While    
+
+    a = int(input("Masukkan angka pertama: "))
+    b = int(input("Masukkan angka kedua: "))
+
+    if Command == "+":
+        print(a + b)
+        break
+    elif Command == "-":
+        print( a - b)
+        break
+    elif Command == "*":
+        print(a * b)
+        break
+    elif Command == "/":
+        print(a / b)
+        break
+
+print("Terima kasih sudah memakai aplikasi saya!")
---
+++ TUGAS/Tes.py
@@ -0,0 +1,6 @@
+X = input("Masukkan: ")
+Y = input("Masukkan: ")
+
+Op = input("Masukkan OP: ")
+
+print(f"")
