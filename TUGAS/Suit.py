fff54e48b2a505d898c7c9f9c2e94220726dd8d9
Azakana <danielsaputradivo@gmail.com>
2024-07-04 17:34:30 +0700

    Mencoba membuat game batu gunting kertas

---
+++ TUGAS/Suit.py
@@ -0,0 +1,15 @@
+
+#! PROGRAM BATU GUNTING KERTAS SEDERHANA WIRR
+
+def alatMain():
+    alat_Main = {
+        "Scissor": "Gunting",
+        "Rock": "Batu",
+        "Papper": "Kertas",
+
+    }
+    print(alat_Main["Scissor"])
+    
+
+alatMain()
+
