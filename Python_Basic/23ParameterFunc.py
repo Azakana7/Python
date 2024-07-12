e7c8246e8e62087e62626972d7cbb8648d4b9002
Azakana <danielsaputradivo@gmail.com>
2024-07-12 17:14:18 +0700

    .

--- Python_Basic/23ParameterFunc.py
+++ Python_Basic/23ParameterFunc.py
@@ -1,2 +1,11 @@
-
+from time import sleep
 #! Durasi: 03:00:03
+def greetings_To_User(Nama):
+    print(f"Halo {Nama}!")
+    print("Selamat Belajar Python!")
+
+greetings_To_User(input("Siapa Nama Anda >>> "))
+sleep(1)
+print("=========================")
+sleep(1)
+greetings_To_User(input("Siapa Nama Palsu Anda >>> ")) #! Dengan Fungsi kita bisa cukup membuat 1 blok koding untuk dipanggil berkali2
