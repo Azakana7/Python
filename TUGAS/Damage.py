d9690033a3fd8757537d57b935ebd9590423f8f9
Azakana7 <danielsaputradivo@gmail.com>
2024-07-17 18:37:27 +0700

    .

--- TUGAS/Damage.py
+++ TUGAS/Damage.py
@@ -7,22 +7,30 @@ Player = {
     "Nama": "divo",
     "Level": 80,
     "HitPoints": 100,
+    "Critical_Bonus": 2
 }
 
 Bot = {
     "Name": "Computer",
     "Lvl": 90,
     "Darah": 150,
+    "Crit_Bonus": 2
 }
 
 
 #Todo: Damage List
 Damage = {
     "Normal": 20,
-    "Super": 60,
-    "Critical": 100
+    "Critical": 60,
+    "Super": 100
 }
 
 #! ====================================Main============================================
 
 Dmg = random.choice(list(Damage.values())) #? Untuk mencetak random index dari Dictionary Damage
+
+print((Player["Critical_Bonus"] * Damage["Critical"]) - Bot["Darah"]   ) 
+
+
+
+
