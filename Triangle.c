619694359c5fd21d2c9fae6036e54c0414a6fb61
Azakana7 <danielsaputradivo@gmail.com>
2024-07-24 09:26:47 +0700

    .

--- Triangle.c
+++ Triangle.c
@@ -1,21 +1,29 @@
 #include <stdio.h>
 
-int main()
+int main() {
+    int a, b, c;
 
-{
+    // Read the values of a, b, and c from the user
+    scanf("%d %d %d", &a, &b, &c);
 
-int a, b, c;
+    // Check if it is not a triangle
+    if ((a >= b + c) || (b >= a + c) || (c >= a + b)) {
+        printf("Bukan Segitiga\n");
+    } else {
+        // Check if it is an equilateral triangle
+        if ((a == b) && (b == c)) {
+            printf("Segitiga Sama Sisi\n");
+        }
+        // Check if it is an isosceles triangle
+        else if ((a == b) || (b == c) || (a == c)) {
+            printf("Segitiga Sama Kaki\n");
+        }
+        // Otherwise, it is a scalene triangle
+        else {
+            printf("Segitiga Sembarang\n");
+        }
+    }
 
-scanf("%d %d %d", &a, &b, &c);
+    return 0;
+}
 
-if ( (a >= b + c) || (b >= a + c) 1(c >= a + b) ) printf("Bukan Segitiga");
-
-else
-
-if (( a ==b) && ( b ==c)) printf("Segitiga Sama Sisi");
-
-else if (( a ==b) || ( b ==c) || ( a ==c)) printf("Segitiga Sama Kaki");
-
-else printf("Segitiga Sembarang");
-
-return 0;
