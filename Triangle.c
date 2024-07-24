fcbafc23725e08e16945268ff2d2c3bd0fc5b5eb
Azakana7 <danielsaputradivo@gmail.com>
2024-07-24 09:09:00 +0700

    .

---
+++ Triangle.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+
+{
+
+int a, b, c;
+
+scanf("%d %d %d", &a, &b, &c);
+
+if ( (a >= b + c) || (b >= a + c) 1(c >= a + b) ) printf("Bukan Segitiga");
+
+else
+
+if (( a ==b) && ( b ==c)) printf("Segitiga Sama Sisi");
+
+else if (( a ==b) || ( b ==c) || ( a ==c)) printf("Segitiga Sama Kaki");
+
+else printf("Segitiga Sembarang");
+
+return 0;
