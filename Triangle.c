dd5f47ccd8445f3515ab600ff498892f492308e3
Azakana7 <danielsaputradivo@gmail.com>
2024-07-25 07:12:28 +0700

    .

--- Triangle.c
+++ Triangle.c
@@ -3,22 +3,22 @@
 int main() {
     int a, b, c;
 
-    // Read the values of a, b, and c from the user
+    
     scanf("%d %d %d", &a, &b, &c);
 
-    // Check if it is not a triangle
+    
     if ((a >= b + c) || (b >= a + c) || (c >= a + b)) {
         printf("Bukan Segitiga\n");
     } else {
-        // Check if it is an equilateral triangle
+        
         if ((a == b) && (b == c)) {
             printf("Segitiga Sama Sisi\n");
         }
-        // Check if it is an isosceles triangle
+        
         else if ((a == b) || (b == c) || (a == c)) {
             printf("Segitiga Sama Kaki\n");
         }
-        // Otherwise, it is a scalene triangle
+        
         else {
             printf("Segitiga Sembarang\n");
         }
