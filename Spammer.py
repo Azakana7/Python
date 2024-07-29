6dde69a0504f70ca1dce685688f0e86a45e93b8d
Azakana7 <danielsaputradivo@gmail.com>
2024-07-29 18:16:22 +0700

    .

---
+++ Spammer.py
@@ -0,0 +1,11 @@
+
+userInput = input("Berapa nomor yang ingin anda serang>>> ") 
+maxAttack = input("Berapa kali anda ingin melakuka")
+
+try:
+    while userInput <= maxAttack:
+        print(f"Sedang menyerang {userInput} ")
+
+    print("penyerangan selesai")
+
+
