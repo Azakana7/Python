eeb5af08957d15803e09d8e6ea8aca673da8f0f6
Azakana <danielsaputradivo@gmail.com>
2024-08-24 13:51:49 +0700

    Update: Menambahkan File Baru & Tutorial Finish!

--- Basic/031Random.py
+++ Basic/031Random.py
@@ -1,3 +1,15 @@
 
 #! Durasi: 03:52:43
 import random
+
+users = ["Agung", "Adit", "Suki"]
+
+batas_bawah = 0
+batas_atas = len(users) - 1
+random_int = random.randint(batas_bawah, batas_atas)
+winner = users[random_int]
+print(winner)
+
+#! for i in range(5):
+#!     rndm = random.randint(batas_bawah, batas_atas)
+#!     print(rndm)
---
+++ Basic/032PIP&ENV.py
@@ -0,0 +1,3 @@
+
+#! Durasi 03:45:59
+#todo: buat nyari package yang kita perlukan untuk proses development kita bisa pakai website: https://pypi.org/
